d27ab77a481ee2b00cbafea64b72a88c4ccfdca3
admin <admin@example.com>
2026-01-19 01:26:56 +0800

    修复CategoryConfig.tsx中的重复placeholder属性问题

--- src/components/admin/CategoryConfig.tsx
+++ src/components/admin/CategoryConfig.tsx
@@ -255,7 +255,7 @@ const CategoryConfig = ({
                 </div>
                 <input
                   type='text'
-                  placeholder='查询条件'
+                  placeholder='例如: category=热门&sort=time'
                   value={newCategory.query}
                   onChange={(e) =>
                     setNewCategory((prev) => ({
@@ -264,7 +264,6 @@ const CategoryConfig = ({
                     }))
                   }
                   className='px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 focus:border-transparent'
-                  placeholder='例如: category=热门&sort=time'
                 />
                 <div className='flex justify-end'>
                   <button
